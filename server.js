9ed0d2ae14be1e714dceaa79a5392cbc7990d0ba
bsteg17 <bsteg17@gmail.com>
2016-03-26 13:15:24 -0500

    recieving access token and logged in user info

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
-express = require('express');
-
-app = express();
+var http = require('http');
+var express = require('express');
+var api = require('instagram-node').instagram();
+var secrets = require('secrets.js');
+var app = express();
 
 CLIENT_ID = 'c0e99f553a0a4a86b026b6923d4e35ef';
-CLIENT_SECRET = 'b0903108c6d44106b6b7994559a79de1';
+CLIENT_SECRET = secrets.client_secret;
 PORT = 8000;
 DOMAIN = 'http://localhost:'+PORT+'/';
-
-app.get('/', function(req, res) {
-    auth_url = 'https://api.instagram.com/oauth/authorize/'+
-    '?client_id='+CLIENT_ID+'&redirect_uri='+(DOMAIN+'game')+'&response_type=code'
-    res.redirect(auth_url);
+ 
+api.use({
+  client_id: CLIENT_ID,
+  client_secret: CLIENT_SECRET
 });
-
-app.get('/game', function(req, res) {
-    if (res.query.error) {
-        // handle auth error
+ 
+var redirect_uri = 'http://localhost:8000/game';
+ 
+exports.authorize_user = function(req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri));
+};
+ 
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function(err, user) {
+    if (err) {
+      console.log(err.body);
+    } else {
+      // Game.enter(user);
     }
-    code = req.query.code;
-    
-});
-
-app.listen(PORT, function() {
-    console.log("listening on port "+PORT);
+  });
+};
+ 
+// This is where you would initially send users to authorize 
+app.get('/', exports.authorize_user);
+// This is your redirect URI 
+app.get('/game', exports.handleauth);
+ 
+http.createServer(app).listen(PORT, function(){
+  console.log("Express server listening on port " + PORT);
 });
